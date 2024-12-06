b4ed9d7514f3143d167f5fb45b63c5d8a69ac9c7
kusshsatija <kusshsatija@gmail.com>
2024-12-05 21:45:12 -0800

    Files for Data, NLP, and Interactive Network

---
+++ NLP/src/constants.py
@@ -0,0 +1,6 @@
+# DATA_PATH = '<path for your resume/job description file>'
+DATA_PATH = '/Users/takshgirdhar/Desktop/COSC421_Project/Job-Description-Parser-master/Resume_JD_parser/input/raw_data.csv'
+# DATA_PATH = '/Users/kusshsatija/Desktop/COSC421/COSC421Project/Job-Description-Parser-master/Resume_JD_parser/input/raw_data.csv'
+# SKILL_FILE_PATH = '<path for the skill-set file>'
+SKILL_FILE_PATH = '/Users/takshgirdhar/Desktop/COSC421_Project/Job-Description-Parser-master/Resume_JD_parser/input/Skillset.xlsx'
+# SKILL_FILE_PATH = '/Users/kusshsatija/Desktop/COSC421/COSC421Project/Job-Description-Parser-master/Resume_JD_parser/input/Skillset.xlsx'
